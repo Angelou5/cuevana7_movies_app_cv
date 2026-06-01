78abd7d86ec2a91c5cd6df0e4f749770fe878224
Angelou5 <tunjafet97@gmail.com>
2026-06-01 11:41:12 -0500

    cambios en docker, implementacion y tabla user en sql

--- lib/domain/repositories/users_repositories.dart
+++ lib/domain/repositories/users_repositories.dart
@@ -4,7 +4,9 @@ import 'package:cuevana7_movies_app_cv/domain/entities/user.dart';
 abstract class UserRepository {
 
   Future<User> signIn(String email, String password);
+
   Future<User> signUp(String name, String email, String password);
+  
   Future<void> signOut();
 
 }
---
+++ lib/implements/datasources/users_datasources.dart
@@ -0,0 +1,49 @@
+import 'package:cuevana7_movies_app_cv/domain/datasources/users_datasources.dart';
+import 'package:cuevana7_movies_app_cv/domain/entities/user.dart';
+import 'package:cuevana7_movies_app_cv/config/db/connect.dart';
+import 'package:postgres/postgres.dart';
+
+
+class UsersDataSourceImpl implements UsersDataSource {
+ 
+   final Future<PostgreSQLConnection> connection = connect(); // humberto
+  @override
+  Future<User> getUser(String userId) async {
+    final conn = await connection;
+    final result = await conn.query(
+      'SELECT id, name, email, password, rol, createdAt, updatedAt FROM users WHERE id = @userId',
+      substitutionValues: {'userId': userId},
+    );
+
+    if (result.isEmpty) {
+      throw Exception('Usuario no encontrado');
+    }
+
+    final row = result.first;
+    return User(id: row[0], name: row[1], email: row[2], password: row[3], rol: row[4], createdAt: row[5], updatedAt: row[6]);
+  }
+  
+  @override
+  Future<bool> saveUser(User user) async {
+    final conn = await connection;
+    final result =await conn.query(
+      r'''
+        INSERT INTO users (id, name, email, password, rol, createdAt, updatedAt)
+        VALUES (@id, @name, @email, @password, @rol, @createdAt, @updatedAt)
+       ''',
+      substitutionValues: {
+        'id': user.id,
+        'name': user.name,
+        'email': user.email,
+        'password': user.password,
+        'rol': user.rol,
+        'createdAt': user.createdAt,
+        'updatedAt': user.updatedAt,
+      },
+    );
+    if(result.affectedRowCount == 0) {
+      throw Exception('Failed to save user');
+    }
+    return true;
+  }
+}
