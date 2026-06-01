dd7f5968807b5c14542627a2109ebc70824a5193
Angelou5 <tunjafet97@gmail.com>
2026-05-31 19:35:22 -0500

    Docker y entidad usuario

---
+++ lib/domain/repositories/user_repositories.dart
@@ -0,0 +1,10 @@
+
+import 'package:cuevana7_movies_app_cv/domain/entities/user.dart';
+
+abstract class UserRepository {
+
+  Future<User> signIn(String email, String password);
+  Future<User> signUp(String name, String email, String password);
+  Future<void> signOut();
+
+}
