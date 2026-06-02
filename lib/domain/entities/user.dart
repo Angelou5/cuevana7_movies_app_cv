f6dbe57f4fd533a7ff785e7e60322dd806ac6bdc
Angelou5 <tunjafet97@gmail.com>
2026-06-02 17:26:13 -0500

    cambio pequeño en entidad

--- lib/domain/entities/user.dart
+++ lib/domain/entities/user.dart
@@ -12,3 +12,29 @@ class UserEntity {
     this.token,
   });
 }
+
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String password;
+  final Roles rol;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  const User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.rol,
+    required this.createdAt,
+    required this.updatedAt
+  });
+}
+
+
+enum Roles {
+
+  user,admin
+}
